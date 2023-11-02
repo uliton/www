302a30de12cb892a43791522f75900ae1c7c7228
Oleksandr Kharutsa <al.haruca@gmail.com>
2023-11-02 22:14:01 +0100

    add react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+
+export const App: React.FC = () => (
+  <div className="app">
+    <Routes>
+      <Route path="/" element={} />
+      <Route path="*" element={} />
+    </Routes>
+  </div>
+);
