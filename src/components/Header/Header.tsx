918036cdaac66e8a40e41c53c89c4c7dacd92c89
Oleksandr Kharutsa <al.haruca@gmail.com>
2023-11-10 16:21:03 +0100

    add filters selector

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,46 @@
-import React, { useContext, useState } from 'react';
-import { Filter } from '../../ui/icons/Filter';
-import { Logo } from '../../ui/logo';
-import { getTranslation, Language } from '../../utils/functions/language';
-import { LanguageSelector } from '../LanguageSelector';
-import { LanguageUsed } from '../LanguageUsed';
+import React, { useState } from 'react';
+import { Logo } from '../../ui/Logo';
+import { FilterButton } from '../FilterButton';
+import { LanguageUsedButton } from '../LanguageUsedButton';
 import style from './Header.module.scss';
+import { FilterSelector } from '../FilterSelector';
+import classNames from 'classnames';
 
 type Props = {
   setLang: (_: string) => void,
 }
 
-enum LANG {
-  en = "en",
-  de = "de",
-  cz = "cz",
-  pl = "pl",
-  ua = "ua",
-  fr = "fr",
-  it = "it",
-  sp = "sp",
-  tr = "tr",
-}
-
 export const Header: React.FC<Props> = ({ setLang }) => {
-  const lang: string = useContext(Language);
-  const MOCK = getTranslation(lang);
+  const [filterSelectorIsOpen, setFilterSelectorIsOpen] = useState<boolean>(false);
 
-  console.log(lang);
-  
   return (
     <div className={style.body}>
-      <div className="container">
-        <div className={style.content}>
-          <div className={style.logo}>
-            <Logo />
+      <div className={style.header}>
+        <div className="container">
+          <div className={style.content}>
+            <div className={style.logo}>
+              <Logo />
+            </div>
+
+            <div className={style.services}>
+              <FilterButton
+                filterSelectorIsOpen={filterSelectorIsOpen}
+                setFilterSelectorIsOpen={setFilterSelectorIsOpen}
+              />
+
+              <LanguageUsedButton setLang={setLang} />
+            </div>
           </div>
 
-          <div className={style.services}>
-            <Filter />
-
-            <LanguageUsed setLang={setLang} />
-          </div>
         </div>
+      </div>
+
+      <div className={classNames({
+        "container": true,
+        [style.filters]: true,
+        [style.filters_is_open]: filterSelectorIsOpen,
+      })}>
+        <FilterSelector filterSelectorIsOpen={filterSelectorIsOpen} />
       </div>
     </div>
   );
